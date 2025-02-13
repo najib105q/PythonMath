8672c1a646cb51228752fb0fc964eacad25de918
Najib <najib.qunoo@student.uni-tuebingen.de>
2025-02-13 22:37:08 +0100

    Correction for the year 2025

--- src/ostern.py
+++ src/ostern.py
@@ -2,13 +2,13 @@ def calculate_easter(year):
     a = year % 19
     b = year % 4
     c = year % 7
-    d = ((19 * a) + 24) % 30
-    e = ((2 * b) +  (4 * c) + (6 * d) + 5) % 7
-    f = 22 + d + e
-    day = f % 32
-    if f > 31 :
-        month = 4
-    else :
-        month = 3
-    return day, month
-print(calculate_easter(2024))
+    d = (19 * a + 24) % 30
+    e = (2 * b + 4 * c + 6 * d + 5) % 7
+    day = 22 + d + e
+
+    if day > 31:
+        return day - 31, 4  
+    else:
+        return day, 3  
+
+print(calculate_easter(2025))  
